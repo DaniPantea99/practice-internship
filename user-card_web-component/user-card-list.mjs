a0868b6ba07208d2a8ab7251cfdf6f4559ce0b60
Daniel Pantea <dani.pantea@gmail.com>
2022-10-21 16:32:42 +0300

    update 21oct

---
+++ user-card_web-component/user-card-list.mjs
@@ -0,0 +1,27 @@
+// export class UserCardList extends HTMLElement {
+//     #userCards = [];
+
+//     constructor() {
+//         super();
+//         this.shadow = this.attachShadow({ mode: 'open' });
+//     }
+
+//     set users(value) {
+//         this.#userCards = value;
+//         this.render();
+//     }
+
+//     render() {
+//         this.shadow.innerHTML = `<div class="users">
+//             ${this.#userCards.map(user => 
+//                 `<user-card 
+//                 picture="${user.picture}"
+//                 username="${user.username}"
+//                 joindate="${user.joindate}"
+//                 email="${user.email}>
+//                 </user-card>`).join('');}
+//         </div>`
+//     }
+// }
+
+// customElements.define('user-card-list', UserCardList);
