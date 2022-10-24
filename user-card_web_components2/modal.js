7771ec1eb8515dbd74951ef00814899c1fa73da8
Daniel Pantea <dani.pantea@gmail.com>
2022-10-24 16:23:46 +0300

    update2 24-oct

--- user-card_web_components2/modal.js
+++ user-card_web_components2/modal.js
@@ -16,14 +16,34 @@ export class Modal extends HTMLElement {
         <h3 class="modal-title">Add new user card</h3>
         <button class="close-modal">X</button>
         </div>
-        
+
         <label for="picture">Add picture url:
-        <input type="text" name="picture" id="picture"></input>
+        <input type="text" name="picture" id="picture" placeholder="https://randomuser.me/api/portraits/lego/1.jpg"></input>
         </label>
         
         <br>
         <label for="username">Name:
-        <input type="text"></input>
+        <input type="text" name="username" id="username" placeholder="New username"></input>
+        </label>
+
+        <br>
+        <label for="email">Email address:
+        <input type="text" name="email" id="email" placeholder="@email"></input>
+        </label>
+
+        <br>
+        <label for="bio">Biography:
+        <textarea rows="3" cols="30" name="bio" id="bio" placeholder="User biography"></textarea>
+        </label>
+
+        <br>
+        <label for="city">City:
+        <input type="text" name="city" id="city" placeholder="City name"></input>
+        </label>
+
+        <br>
+        <label for="link">HTTP address:
+        <input type="text" name="link" id="link" placeholder="URL address"></input>
         </label>
         
         </div>
